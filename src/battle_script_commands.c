913400df4b2a62182c87768f231bfffe130f0afa
ander-vieira <ander.vieira741@gmail.com>
2025-02-26 18:08:54 +0100

    Update Wrap mechanics (1/8 damage per turn, for 4 turns)

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -2458,7 +2458,7 @@ void SetMoveEffect(bool8 primary, u8 certain)
                 }
                 else
                 {
-                    gBattleMons[gEffectBattler].status2 |= STATUS2_WRAPPED_TURN((Random() & 3) + 3); // 3-6 turns
+                    gBattleMons[gEffectBattler].status2 |= STATUS2_WRAPPED_TURN(5); // 4 turns
 
                     *(gBattleStruct->wrappedMove + gEffectBattler * 2 + 0) = gCurrentMove;
                     *(gBattleStruct->wrappedMove + gEffectBattler * 2 + 1) = gCurrentMove >> 8;
