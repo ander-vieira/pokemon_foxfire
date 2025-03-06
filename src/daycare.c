499809fb11736f456265d0b78f1c1684a0e3aae5
ander-vieira <ander.vieira741@gmail.com>
2025-03-06 21:06:44 +0100

    Allow NidoranM and Volbeat to make eggs of the opposite gender with Ditto

--- src/daycare.c
+++ src/daycare.c
@@ -1021,14 +1021,14 @@ static u16 DetermineEggSpeciesAndParentSlots(struct DayCare *daycare, u8 *parent
     }
 
     eggSpecies = GetEggSpecies(species[parentSlots[0]]);
-    if (eggSpecies == SPECIES_NIDORAN_F && daycare->offspringPersonality & EGG_GENDER_MALE)
-    {
+    if (eggSpecies == SPECIES_NIDORAN_F && (daycare->offspringPersonality & EGG_GENDER_MALE))
         eggSpecies = SPECIES_NIDORAN_M;
-    }
-    if (eggSpecies == SPECIES_ILLUMISE && daycare->offspringPersonality & EGG_GENDER_MALE)
-    {
+    if (eggSpecies == SPECIES_NIDORAN_M && !(daycare->offspringPersonality & EGG_GENDER_MALE))
+        eggSpecies = SPECIES_NIDORAN_F;
+    if (eggSpecies == SPECIES_ILLUMISE && (daycare->offspringPersonality & EGG_GENDER_MALE))
         eggSpecies = SPECIES_VOLBEAT;
-    }
+    if (eggSpecies == SPECIES_VOLBEAT && !(daycare->offspringPersonality & EGG_GENDER_MALE))
+        eggSpecies = SPECIES_ILLUMISE;
 
     // Make Ditto the "mother" slot if the other daycare mon is male.
     if (species[parentSlots[1]] == SPECIES_DITTO && GetBoxMonGender(&daycare->mons[parentSlots[0]].mon) != MON_FEMALE)
