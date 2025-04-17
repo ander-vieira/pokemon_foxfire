8672463c66a9d9ee576579f9e781b44a54016364
ander-vieira <ander.vieira741@gmail.com>
2025-04-17 15:11:02 +0200

    Remove rival scene in Four Island

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -164,7 +164,7 @@
 #define FLAG_HIDE_RESORT_GORGEOUS_SELPHY                   0x094
 #define FLAG_HIDE_RESORT_GORGEOUS_INSIDE_SELPHY            0x095
 #define FLAG_HIDE_SELPHYS_BUTLER                           0x096
-#define FLAG_HIDE_FOUR_ISLAND_RIVAL                        0x097
+#define FLAG_HIDE_FOUR_ISLAND_RIVAL                        0x097//UNUSED
 #define FLAG_HIDE_SIX_ISLAND_POKECENTER_RIVAL              0x098
 #define FLAG_HIDE_DEOXYS                                   0x099
 #define FLAG_HIDE_BIRTH_ISLAND_METEORITE                   0x09A
